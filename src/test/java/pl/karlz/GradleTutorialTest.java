b00451f06d41dec7d7ebd757d2e1fd42e074b6ea
Karlz-Bandz <57764322+Karlz-Bandz@users.noreply.github.com>
2022-06-17 21:02:42 +0200

    first commit

---
+++ src/test/java/pl/karlz/GradleTutorialTest.java
@@ -0,0 +1,12 @@
+package pl.karlz;
+
+//import org.junit.test;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+    @Test
+    public void verifyNoExceptionThrow() {
+        GradleTutorial.main(new String[]{});
+    }
+}
